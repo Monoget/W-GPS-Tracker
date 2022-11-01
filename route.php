e345f4603a90d984f9cb6d1568ced9af6f370933
Monoget <monoget2@gmail.com>
2022-11-01 09:37:49 +0600

    update db

--- route.php
+++ route.php
@@ -12,6 +12,11 @@ $db_handle = new DBController();
     <title>Route | GPS Tracker</title>
     <!-- Favicon icon -->
     <?php require_once('includes/css.php'); ?>
+    <style>
+        #map{
+            height: 600px;
+        }
+    </style>
 </head>
 <body>
 
@@ -59,7 +64,10 @@ $db_handle = new DBController();
         <!-- row -->
         <div class="container-fluid">
             <div class="row">
-
+                <div class="col-12">
+                    <div id="map">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -93,7 +101,36 @@ $db_handle = new DBController();
     Scripts
 ***********************************-->
 <!-- Required vendors -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQiD8S2lGvDu_YwNxHvOCI1Ozp7n6DbH8&callback=initMap" type="text/javascript"></script>
 <?php require_once('includes/js.php'); ?>
+<script>
+    x = navigator.geolocation;
 
+    x.getCurrentPosition(success, failure);
+
+    function success(position)
+
+    {
+        let myLat = position.coords.latitude;
+        let myLong = position.coords.longitude;
+
+        //alert(myLat);
+
+        let coords = new google.maps.LatLng(myLat,myLong);
+
+        let mapOptions = {
+
+            zoom:15,
+            center: coords,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        let marker = new google.maps.Marker({map:map, position:coords});
+    }
+
+    function failure(){ }
+
+</script>
 </body>
 </html>
